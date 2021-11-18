aee5220019785cd9d49249d70ff84c9330aeee5d
Shubhamrawat5 <sr856161@gmail.com>
2021-11-18 23:41:08 +0530

    add donators

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -1,8 +1,70 @@
 import React from "react";
 import certiImg from "../asserts/images/pvx/certi.jpg";
 import qrImg from "../asserts/images/pvx_qr.png";
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
 
-export default function Others() {
+export default function Donate() {
+  const isUnmounted = useRef(false);
+  const [donators, setdonators] = useState([]);
+  const [totalDonation, setTotalDonation] = useState(0);
+  const [donatorsInfo, setdonatorsInfo] = useState("");
+
+  useEffect(() => {
+    if (isUnmounted.current) return;
+    //TODO: FIX THIS IF
+    if (donators.length !== 0) return; //to avoid reattaching donators
+
+    setdonatorsInfo("Adding donator data... please wait.");
+    const url = "https://pvxgroup.herokuapp.com/api/donation";
+    const urlBackup = "https://pvxgroupbackup.herokuapp.com/api/donation";
+
+    function setDonatorsList({ data }) {
+      if (isUnmounted.current) return;
+      let total = 0;
+      data.sort((x, y) => y.amount - x.amount);
+      setdonators(data);
+      data.forEach((ele) => {
+        total += ele.amount;
+      });
+      setTotalDonation(total);
+    }
+
+    async function tryWithUrl(url) {
+      // try {
+      let { data } = await axios.get(url);
+      setDonatorsList(data);
+
+      setdonatorsInfo("Donators data added!");
+      setTimeout(() => {
+        setdonatorsInfo("");
+        return true;
+      }, 2000);
+    }
+
+    async function start() {
+      let response = await tryWithUrl(url);
+      if (response === false) {
+        //error in main url, now trying backup url
+        console.log("Error in main url.. trying backup url.");
+        setdonatorsInfo("Problem with main url.. trying backup url.");
+        let response = await tryWithUrl(urlBackup);
+        if (response === false) {
+          setdonatorsInfo(
+            "NOTE: There is a problem with attaching the donators data ! Contact PVX admins."
+          );
+        }
+      }
+    }
+
+    start();
+    return () => {
+      isUnmounted.current = true;
+    };
+    //TODO: fix this eslint warning
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <section id="achieve-section" className="section">
       <h2 className="section-heading">DONATE</h2>
@@ -18,8 +80,8 @@ export default function Others() {
       <div className="drive-container">
         <p className="donation-text">
           <span className="step">
-            1) Scan the given qr code or donate at shubhamraw123@okhdfcbank this
-            VPA using your favourite payments app.
+            1) Scan the given qr code or donate at "shubhamraw123@okhdfcbank"
+            this VPA using your favourite payments app.
             <br />
           </span>
           <span className="step">
@@ -35,7 +97,7 @@ export default function Others() {
             <br />
           </span>
           <span className="step">
-            3) You'll be listed on community bot, website and other products as
+            3) You'll be listed on community bot, website and other apps as
             donor 💰
           </span>
           <br />
@@ -47,9 +109,34 @@ export default function Others() {
       <br />
 
       <p className="drive-subheading">
-        Donations are ✅ transparently used to build tools, 🌐 host website, 🤖
+        Donations are ✅ transparently used to build tools, 🌐 buy domain, 🤖
         bots and much more ❤️
       </p>
+
+      {donatorsInfo ? (
+        <div id="err" className="err">
+          {donatorsInfo}
+        </div>
+      ) : null}
+
+      {donators.length !== 0 ? (
+        <>
+          <h2 className="donators-subheading">
+            <span>TOTAL DONATIONS:</span>
+            <span>{"₹ " + totalDonation}</span>
+          </h2>
+          <table className="donators-table">
+            <tbody>
+              {donators.map((mem, index) => (
+                <tr key={index}>
+                  <td className="donator-name">{mem.name}</td>
+                  <td className="donator-amount">{"₹ " + mem.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      ) : null}
 
       <div className="achieve-box donation">
         <img src={certiImg} alt="certificate" />
