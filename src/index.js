ed115d744760fcdba59989ec105046163f6412be
Shubhamrawat5 <sr856161@gmail.com>
2021-11-26 23:41:10 +0530

    remove pvx animator in start

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import ReactDOM from "react-dom";
 import "./index.css";
-import React, { lazy, Suspense } from "react";
-import Loader from "./components/Loader";
+import React from "react";
+// import { lazy, Suspense } from "react";
+// import Loader from "./components/Loader";
+import App from "./App";
 
 //minimum given time it will take to load App component (for showing loading loader everytime)
-const App = lazy(() => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(import("./App")), 2000);
-  });
-});
+// const App = lazy(() => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(import("./App")), 2000);
+//   });
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<Loader />}>
-      <App />
-    </Suspense>
+    {/* <Suspense fallback={<Loader />}> */}
+    <App />
+    {/* </Suspense> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
